a7b337cfe8ccada1bd3594e2484521d209830357
emma <57380607@qq.com>
2019-02-14 22:19:03 +0800

    init

---
+++ src/MvcApp.Web/App_Start/MvcConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcApp.Web
+{
+    public class MvcConfig
+    {
+        public static void Init()
+        {
+            RegisterRoutes(RouteTable.Routes);
+            RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+
+        private static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default_Root",
+                url: "",
+                defaults: new { controller = "Home", action = "Index"}
+            );
+
+            routes.MapRoute(
+                name: "Default_Common",
+                url: "Common/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+        private static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            //filters.Add(new MyHandleErrorAttribute());
+        }
+    }
+}
