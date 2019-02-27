d0df4bb80acae90e7346fc00ccb1abc695177a10
congzw <2981810300@qq.com>
2019-02-27 12:53:42 +0800

    add ace support

---
+++ src/CommonFx/DateHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CommonFx
+{
+    public interface IDateHelper
+    {
+        /// <summary>
+        /// 获取系统时间
+        /// </summary>
+        /// <returns></returns>
+        DateTime Now();
+    }
+
+    public class DateHelper : IDateHelper
+    {
+        #region for di extensions
+
+        private static Func<IDateHelper> _resolve = () => ResolveAsSingleton.Resolve<DateHelper, IDateHelper>();
+        public static Func<IDateHelper> Resolve
+        {
+            get { return _resolve; }
+            set { _resolve = value; }
+        }
+
+        #endregion
+
+        public DateTime Now()
+        {
+            return DateTime.Now;
+        }
+        
+        #region for simple use
+
+        /// <summary>
+        /// 获取当前时间
+        /// </summary>
+        /// <returns></returns>
+        public static DateTime GetNow()
+        {
+            return Resolve().Now();
+        }
+
+        #endregion
+    }
+}
---
+++ src/MvcApp.AppLib.Common/LayoutHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace MvcApp.AppLib.Common
+{
+    public class LayoutHelper
+    {
+        public LayoutHelper()
+        {
+            BasicLayout = "~/Views/Shared/_Basic/_Layout.cshtml";
+            AceLayout = "~/Views/Shared/_Ace/_AceLayout.cshtml";
+            UnifyLayout = "~/Views/Shared/_Unify/_UnifyLayout.cshtml";
+        }
+
+        public string BasicLayout { get; set; }
+        public string AceLayout { get; set; }
+        public string UnifyLayout { get; set; }
+
+
+        public static LayoutHelper Intance = new LayoutHelper();
+    }
+}
